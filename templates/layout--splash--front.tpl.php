e370dd3f19faeb251c32f01bb9b210675b35cab8
Tim Erickson (@stpaultim) <tim@stpaultim.com>
2024-06-29 02:55:47 -0500

    More themeing

---
+++ templates/layout--splash--front.tpl.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @file
+ * Template for the Splash layout.
+ *
+ * Variables:
+ * - $title: The page title, for use in the actual HTML content.
+ * - $messages: Status and error messages. Should be displayed prominently.
+ * - $tabs: Tabs linking to any sub-pages beneath the current page
+ *   (e.g., the view and edit tabs when displaying a node.)
+ * - $action_links: Array of actions local to the page, such as 'Add menu' on
+ *   the menu administration interface.
+ * - $classes: Array of CSS classes to be added to the layout wrapper.
+ * - $attributes: Array of additional HTML attributes to be added to the layout
+ *     wrapper. Flatten using backdrop_attributes().
+ * - $content: An array of content, each item in the array is keyed to one
+ *   region of the layout. This layout supports the following sections:
+ *   - $content['header']
+ *   - $content['top']
+ *   - $content['content']
+ *   - $content['bottom']
+ *   - $content['footer']
+ */
+?>
+<div class="layout--splash <?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+
+  <?php if ($content['header']): ?>
+    <header class="l-header region" role="banner" aria-label="<?php print t('Site header'); ?>">
+      <div class="l-header-inner container container-fluid">
+        <?php print $content['header']; ?>
+      </div>
+    </header>
+  <?php endif; ?>
+
+  <div class="l-wrapper">
+
+      <?php if (!empty($content['top'])): ?>
+        <div class="l-top region">
+            <?php print $content['top']; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($messages): ?>
+        <div class="l-messages" role="status" aria-label="<?php print t('Status messages'); ?>">
+          <?php print $messages; ?>
+        </div>
+      <?php endif; ?>
+
+      <div class="l-page-title">
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+          <h1 class="page-title"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+      </div>
+
+      <?php if ($tabs): ?>
+        <nav class="tabs" role="tablist" aria-label="<?php print t('Admin content navigation tabs.'); ?>">
+          <?php print $tabs; ?>
+        </nav>
+      <?php endif; ?>
+
+      <?php print $action_links; ?>
+
+      <div class="l-content region" role="main" aria-label="<?php print t('Main content'); ?>">
+        <div class="l-content-inner container container-fluid">
+          <?php print $content['content']; ?>
+        </div>
+      </div>
+
+      <?php if (!empty($content['bottom'])): ?>
+      <div class="l-bottom region">
+        <div class="l-bottom-inner container container-fluid">         
+          <?php print $content['bottom']; ?>
+        </div>
+      </div>
+      <?php endif; ?>
+  </div><!-- /.l-wrapper -->
+
+  <?php if ($content['footer']): ?>
+    <footer class="l-footer region">
+      <div class="l-footer-inner container container-fluid">
+        <?php print $content['footer']; ?>
+      </div><!-- /.container -->
+    </footer>
+  <?php endif; ?>
+</div><!-- /.layout--splash -->
