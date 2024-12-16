c2e25db3ddb735499538926d4bb286bf93854cf0
Tim Erickson <tim@stpaultim.com>
2024-12-15 22:29:47 -0600

    Remove "Hello World" message from .js file"

--- js/digital_agency.js
+++ js/digital_agency.js
@@ -2,13 +2,3 @@
  * @file
  * Custom javascript for the MyModule module.
  */
-
-(function($) {
-  Backdrop.behaviors.myModule = {
-    attach: function(context, settings) {
-
-      console.log('Hello world!');
-
-    }
-  };
-})(jQuery);
